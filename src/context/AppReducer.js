c520c0c83d018e6016856ed9cd1e21e581577b4a
mikeypy <michaelakins@hotmail.co.uk>
2021-01-08 13:01:10 +0000

    awesome changes

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,15 +1,12 @@
 export const AppReducer = (state, action) => {
   switch(action.type) {
     case 'DELETE_TRANSACTION':
-      return {
-        ...state,
-        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-      }
+      return state.filter(transaction => transaction.id !== action.payload)
+      
       case 'ADD_TRANSACTION':
-      return {
-        ...state,
-        transactions: [action.payload,...state.transactions]
-      }
+      return [
+        ...state, action.payload
+      ]
 
     default:
       return state;
